3699f2e6dd04220f0a1a10b06c20187f9ee4f800
palaknigam03 <95473476+palaknigam03@users.noreply.github.com>
2022-05-18 15:30:23 +0530

    work

--- src/app/storage/storage-routing.module.ts
+++ src/app/storage/storage-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddItemsComponent } from './add-items/add-items.component';
 import { AddStorageComponent } from './add-storage/add-storage.component';
 
 const routes: Routes = [
-  {path:'',component:AddStorageComponent}
+  //{path:'',component:AddStorageComponent}
+  {path:'',component:AddItemsComponent}
 ];
 
 @NgModule({
--- src/app/storage/storage.module.ts
+++ src/app/storage/storage.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { StorageRoutingModule } from './storage-routing.module';
 import { AddStorageComponent } from './add-storage/add-storage.component';
 import { FormsModule } from '@angular/forms';
+import { AddItemsComponent } from './add-items/add-items.component';
 
 
 @NgModule({
   declarations: [
-    AddStorageComponent
+    AddStorageComponent,
+    AddItemsComponent
   ],
   imports: [
     CommonModule,
