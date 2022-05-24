17e9509a6eddc3f5bfc8835c8839e0c3b5ba73f0
Shriram Mishra <ram.techcare27@gmail.com>
2022-05-24 18:07:16 +0530

    *

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ app.use(express.static('./dist/Frontend_Admin'));
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: 'dist/Frontend_Admin' })
 });
+app.use((req, res) => {
+    res.sendFile('index.html', { root: 'dist/Frontend_Admin' });
+});
 app.listen(process.env.PORT || 8080);
