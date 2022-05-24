5d057cad589dfed6a4fd61c1ec34b29bd448ddd3
Shriram Mishra <ram.techcare27@gmail.com>
2022-05-24 18:25:30 +0530

    change

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-const express = require("express");
-const path = require("path");
+const express = require('express')
+const path = require('path');
 const app = express();
-app.use(express.static('./dist/Frontend_Admin'));
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: 'src' })
-});
-app.use((req, res) => {
-    res.sendFile('index.html', { root: 'src' });
-});
+app.use(express.static(__dirname + '/dist/frontend-admin'));
+app.get('/', function(req, res) { res.sendFile(path.join(__dirname + 'index.html')); });
 app.listen(process.env.PORT || 8080);
